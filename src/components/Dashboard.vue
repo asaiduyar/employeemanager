aa846336c8a231df62da56a40285d7186dffdd75
asaiduyar <asaiduyar@gmail.com>
2021-06-23 15:18:48 +0300

    image-passive-active adding, alignment problematic

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,16 +2,53 @@
   <div id="dashboard">
       <ul class="collection with-header">
         <li class="collection-header">
-          <h4>Employees</h4>
+          
+          <b-container class="bv-example-row">
+            <b-row>
+              <b-col>
+                <h3>Active Employees</h3>
+              
+                <li v-for="employee in employees" v-bind:key="employee.id" class="collection-item">
+                 <b-container>
+                 <b-col>
+                    <b-row v-if="employee.status == 'active'" class="col-ex">
+                     <img :src="employee.image" alt="" class="avatar">
+                     <div class="chip">{{employee.dept}}</div>
+                     {{employee.employee_id}}:{{employee.name}}
+                     <router-link class="secondary-content" 
+                       v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
+                      <i class="fa fa-eye" aria-hidden="true"></i>
+                    </router-link>
+                   </b-row>
+                  </b-col>
+                </b-container>
+               </li>
+              </b-col>
+
+              <b-col>
+                <h3>Passive Employees</h3>
+                <li v-for="employee in employees" v-bind:key="employee.id" class="collection-item">
+                 <b-container>
+                 <b-col>
+                    <b-row v-if="employee.status == 'passive'" class="col-ex">
+                     <img :src="employee.image" alt="" class="avatar">
+                     <div class="chip">{{employee.dept}}</div>
+                     {{employee.employee_id}}:{{employee.name}}
+                     <router-link class="secondary-content" 
+                       v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
+                      <i class="fa fa-eye" aria-hidden="true"></i>
+                    </router-link>
+                    </b-row>
+                 </b-col>
+                </b-container>
+               </li>         
+              </b-col>
+
+            </b-row>
+          </b-container>
         </li>
-        <li v-for="employee in employees" v-bind:key="employee.id" class="collection-item">
-          <div class="chip">{{employee.dept}}</div>
-          {{employee.employee_id}}:{{employee.name}}
-          <router-link class="secondary-content" 
-          v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
-          <i class="fa fa-eye" aria-hidden="true"></i>
-          </router-link>
-        </li>
+        
+          
       </ul>
 
 
@@ -43,7 +80,9 @@ export default {
             "employee_id": doc.data().employee_id,
             "name": doc.data().name,
             "dept": doc.data().dept,
-            "position": doc.data().position
+            "position": doc.data().position,
+            "image": doc.data().image,
+            "status": doc.data().status
           }
           this.employees.push(data)
         })
@@ -53,5 +92,12 @@ export default {
 </script>
 
 <style>
-
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50% ;
+}
+.col-ex {
+  padding-block-end: 10%;
+}
 </style>
