bd254830f8e8a131692bd0efc6840a85884d1371
asaiduyar <asaiduyar@gmail.com>
2021-06-23 12:03:13 +0300

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDcV4KTnqQwaCWzACWgyXbZxMB5C9xN4RY",
+    authDomain: "vuefs-sim.firebaseapp.com",
+    projectId: "vuefs-sim",
+    storageBucket: "vuefs-sim.appspot.com",
+    messagingSenderId: "1054350333375",
+    appId: "1:1054350333375:web:674e5a8f67034602fefd0a",
+    measurementId: "G-3ZKP5D9MRX"
+}
