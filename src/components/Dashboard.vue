7e024a40ab5046f80705842e5407e599d7e22fed
asaiduyar <asaiduyar@gmail.com>
2021-06-30 18:20:02 +0300

    MakepPassiveActive-Yedek-Responsive düzenleme

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,8 +3,8 @@
     
     <b-container fluid class="bv-example-row">
       <b-row>
-        <b-col cols="2">
-          <label for="after-input">Show Data(s) after this date</label>
+        <b-col cols="5" md="auto">
+          <label for="after-input" style="font-size:10px">Show Data(s) after this date</label>
             <b-form-datepicker
               id="after-input"
               v-model="after_value"
@@ -16,8 +16,8 @@
               size="sm"
             ></b-form-datepicker>
         </b-col>
-        <b-col cols="2">
-          <label for="after-input">Show Data(s) after this time</label>
+        <b-col cols="5" md="auto">
+          <label for="after-input" style="font-size:10px">Show Data(s) after this time</label>
             <b-form-timepicker
               id="timepicker-after"
               v-model="after_value_time"
@@ -30,8 +30,8 @@
         <b-col>
 
         </b-col>
-        <b-col cols="2">
-          <label for="before-input">Show Data(s) before this date</label>
+        <b-col cols="5" md="auto">
+          <label for="before-input" style="font-size:10px">Show Data(s) before this date</label>
             <b-form-datepicker
               id="before-input"
               v-model="before_value"
@@ -43,8 +43,8 @@
               size="sm"
             ></b-form-datepicker>
         </b-col>
-        <b-col cols="2">
-          <label for="before-input">Show Data(s) before this time</label>
+        <b-col cols="5" md="auto">
+          <label for="before-input" style="font-size:10px">Show Data(s) before this time</label>
             <b-form-timepicker
               id="timepicker-before"
               v-model="before_value_time"
@@ -71,14 +71,21 @@
                             {{employee.employee_id}}:{{employee.name}}
                          </div>
                         <div class="col-sm-4">
-                          <div style="font-size:12px">
-                            {{employee.regDate}}
-                          </div>
+                          <b-col>
+                              <div style="font-size:11px">
+                                {{employee.regDate}}
+                              </div>
+                          </b-col>  
+                          <b-col>
+                            <div>
+                              <b-button v-on:click="makeChangeStat(employee.employee_id, employee.status)" pill variant="outline-danger" size="sm">Make-Passive</b-button>
+                            </div>
                             <router-link class="secondary-content" 
-                            v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
-                            <i class="fa fa-eye" aria-hidden="true"></i>
+                              v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
+                              <i class="fa fa-eye" aria-hidden="true"></i>
                             </router-link>
-                        </div>
+                          </b-col>
+                       </div>
                   </b-row>
                 </ul>
               </b-col>
@@ -96,32 +103,41 @@
                         {{employee.employee_id}}:{{employee.name}}
                      </div>
                      <div class="col-sm-4">
-                       <div id="demo" style="font-size:12px">
-                         {{employee.regDate}}
-                       </div>
-                       <router-link class="secondary-content" 
-                        v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
-                        <i class="fa fa-eye" aria-hidden="true"></i>
-                        </router-link>
+                       <b-col>
+                          <div style="font-size:11px">
+                            {{employee.regDate}}
+                          </div>
+                       </b-col>  
+                       <b-col>
+                            <div>
+                              <b-button v-on:click="makeChangeStat(employee.employee_id, employee.status, employee.name)" pill variant="outline-danger" size="sm">Make-Active</b-button>
+                            </div>
+                            <router-link class="secondary-content" 
+                              v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
+                              <i class="fa fa-eye" aria-hidden="true"></i>
+                            </router-link>
+                          </b-col>
                     </div>  
                   </b-row>   
                  </ul>   
               </b-col>
            
       </a-tab-pane>
-    </a-tabs>
 
-<!--
-    <select name="LeaveType" @change="onContext()" class="form-control" v-model="selected_key">
-      <option disabled value="">Please select one</option>
-      <option value="-1">Hepsi</option>
-      <option value="2">2</option>
-      <option value="10">10</option>
-      <option value="20">20</option>
-      <option value="50">50</option>
-    </select>
-    <span>Selected: {{ selected_key }}</span>
--->
+    </a-tabs>
+    <b-row>
+      <b-col md="auto">
+         <select name="LeaveType" @change="onContext()" class="form-control" v-model="selected_key">
+          <option disabled value="">Göster (adet)</option>
+          <option value="-1">Hepsi</option>
+          <option value="2">2</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+        </select>
+      </b-col>
+    </b-row>
+
       <div class="fixed-action-btn">
           <router-link to="/new">
             <a href="#" class="btn-floating btn-large waves-effect waves-light red">
@@ -138,11 +154,10 @@ import $ from 'jquery'
 
 export default {
     name: "dashboard",
+    
     data () {
         return {
           componentKey: 0,
-          active_employees: [],
-          passive_employees: [],
           filteredActive: [],
           filteredPassive: [],
           after_value: "",
@@ -150,8 +165,8 @@ export default {
           after_value_time: "",
           before_value_time: "",
           selected_key: "",
-          count_active: 0,
-          count_passive: 0
+          spec_employee: null,
+          temp: []
         }
     },
 
@@ -165,11 +180,23 @@ export default {
 
         onContext() {
 
-            this.count_active = 0
-            this.count_passive = 0
-            var filterNum = this.selected_key
+            var limit_size = this.selected_key
+            var collection_name = "employees"
+
+            if (limit_size < 0 || limit_size == ""){
+
+                this.writeFS(collection_name)
+            }
+            else if (limit_size >= 1) {
+
+              this.writeFSLimit(collection_name, limit_size)
+            }
             
-            
+    },
+
+
+       writeFS (collection_name) {
+
             var minutes_after = this.after_value_time.substring(3,5)
             var hours_after = this.after_value_time.substring(0,2)
             var minutes_before = this.before_value_time.substring(3,5)
@@ -182,10 +209,10 @@ export default {
             var before_timestamp_added = this.addTimetoDate(before_timestamp, hours_before, minutes_before)
 
 
-            db.collection("employees").orderBy("dept").onSnapshot(snapshot => {
+          db.collection(collection_name).orderBy("regTimeStamp").limit().onSnapshot(snapshot => {
+             this.filteredActive = []
+             this.filteredPassive = []
             snapshot.forEach(doc => {
-                this.filteredActive = []
-                this.filteredPassive = []
              const data = {
                 "id": doc.id,
                 "employee_id": doc.data().employee_id,
@@ -197,41 +224,38 @@ export default {
                 "regDate": doc.data().regDate,
                 "regTimeStamp": doc.data().regTimeStamp
              }
-            
-            
-               if( !(data.regTimeStamp < after_timestamp_added) && !(data.regTimeStamp > before_timestamp_added) && (data.status == "active")) 
-                  {
-                     this.filteredActive.push(data)
-                  }
-               else if ( !(data.regTimeStamp < after_timestamp_added) && !(data.regTimeStamp > before_timestamp_added) && data.status == "passive")
-                 {
-                     this.filteredPassive.push(data)
-                 }
-            
-            
+             
+            if( !(data.regTimeStamp < after_timestamp_added) && !(data.regTimeStamp > before_timestamp_added) && data.status == "active") {
+               this.filteredActive.push(data)
+            }
+            else if ( !(data.regTimeStamp < after_timestamp_added) && !(data.regTimeStamp > before_timestamp_added) && data.status == "passive"){
+               this.filteredPassive.push(data)}
             })
             
         })
-          console.log(this.filteredActive)
-    },
 
-      
-      forceRerender () {
-            this.componentKey += 1;
-    },
+       },
 
-      callback(key) {
-            console.log(key)
-        }
-    },
 
-    mounted () {
+        // override writeFS
+       writeFSLimit (collection_name, limit_size) {
 
-        db.collection("employees").orderBy("dept").onSnapshot(snapshot => {
-          this.active_employees = []
-          this.passive_employees = []
-          snapshot.forEach(doc => {
-              const data = {
+            var minutes_after = this.after_value_time.substring(3,5)
+            var hours_after = this.after_value_time.substring(0,2)
+            var minutes_before = this.before_value_time.substring(3,5)
+            var hours_before = this.before_value_time.substring(0,2)
+            
+            var after_timestamp = new Date(this.after_value).getTime()
+            var before_timestamp = new Date(this.before_value).getTime()
+
+            var after_timestamp_added = this.addTimetoDate(after_timestamp, hours_after, minutes_after)
+            var before_timestamp_added = this.addTimetoDate(before_timestamp, hours_before, minutes_before)
+
+
+          db.collection(collection_name).where("status", "==", "active").orderBy("regTimeStamp").limit(limit_size).onSnapshot(snapshot => {
+             this.filteredActive = []
+            snapshot.forEach(doc => {
+             const data = {
                 "id": doc.id,
                 "employee_id": doc.data().employee_id,
                 "name": doc.data().name,
@@ -242,15 +266,74 @@ export default {
                 "regDate": doc.data().regDate,
                 "regTimeStamp": doc.data().regTimeStamp
              }
-            if(data.status === "active") {
-               this.active_employees.push(data)
+             
+            if( !(data.regTimeStamp < after_timestamp_added) && !(data.regTimeStamp > before_timestamp_added)) {
+               this.filteredActive.push(data)
             }
-            else {this.passive_employees.push(data)}
-         })
+        })
       })
-      
-      forceRerender()
-      
+
+          db.collection(collection_name).where("status", "==", "passive").orderBy("regTimeStamp").limit(limit_size).onSnapshot(snapshot => {
+             this.filteredPassive = []
+            snapshot.forEach(doc => {
+             const data = {
+                "id": doc.id,
+                "employee_id": doc.data().employee_id,
+                "name": doc.data().name,
+                "dept": doc.data().dept,
+                "position": doc.data().position,
+                "image": doc.data().image,
+                "status": doc.data().status,
+                "regDate": doc.data().regDate,
+                "regTimeStamp": doc.data().regTimeStamp
+             }
+             
+            if( !(data.regTimeStamp < after_timestamp_added) && !(data.regTimeStamp > before_timestamp_added)) {
+               this.filteredPassive.push(data)
+            }
+        })
+      })
+        
+       }, 
+ 
+       
+       makeChangeStat (emp_id, stat, name) {
+          
+          var changeDate = new Date().toLocaleString()
+          var changeTimeStamp = Date.now()
+
+  
+        if (stat == "active"){
+          stat = "passive"
+        }
+        else {
+          stat = "active"
+        }
+
+        db.collection("employees")
+            .where("employee_id", "==", emp_id).get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    doc.ref.update({
+                        status: stat
+                    })
+                })
+            })
+        
+        db.collection("yedek").add({
+                "employee_id": emp_id,
+                "name": name,
+                "old-status": stat,
+                "changeDate": changeDate,
+                "changeTimeStamp": changeTimeStamp
+        })
+
+    },
+
+
+      callback(key) {
+            console.log(key)
+        }
     }
 
 }
