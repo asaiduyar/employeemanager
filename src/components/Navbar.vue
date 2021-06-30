569285726554dd7f4875729344ac37b96d04d5eb
asaiduyar <asaiduyar@gmail.com>
2021-06-30 18:19:11 +0300

    MakePassive/Active-Yedek-Responsive düzenleme

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,14 @@
   <nav>
       <div class="nav-wrapper green">
           <div class="container">
-              <router-link to="/" class="brand-logo">
+              <router-link to="/" class="brand-logo left">
               Employee Manager
               </router-link>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+              </ul>
           </div>
       </div>
   </nav>
